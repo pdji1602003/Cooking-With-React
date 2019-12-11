77847637b66cb8b3e287ea76adc06fc75feb6a37
pdji1602003 <pdji1602003@gmail.com>
2019-12-11 16:04:29 +0800

    cooking with react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
+import React from 'react'
+import RecipeList from './RecipeList'
 
-export const ThemeContext = React.createContext()
-
-function App() {
-	const [theme, setTheme] = useState('red')
+export default function App(props) {
 	return (
-		<ThemeContext.Provider value={{ backgroundColor: theme }}>
-			Counter:
-			<Counter initialCount={0} />
-			CounterHooks:
-			<CounterHooks initialCount={0} />
-			<button onClick={() => { setTheme(prevTheme => prevTheme === 'red' ? 'blue' : 'red') }}>Toggle Theme</button>
-		</ThemeContext.Provider>
+		<>
+			<RecipeList recipes={sampleRecipes}/>
+		</>
 	)
 }
 
-export default App
+const sampleRecipes = [
+	{
+		id:1, 
+		name:'Plain Chicken',
+		cookTime:'1:45',
+		servings:3,
+		instructions:`
+		1. Put salt on chicken 
+		2. Put chicken in oven 
+		3. Eat chicken`
+	},
+	{
+		id:2, 
+		name:'Plain Pork',
+		cookTime:'0:45',
+		servings:5,
+		instructions:`
+		1. Put paprika on pork
+		2. Put chicken in oven 
+		3. Eat pork`
+	}
+]
---
+++ src/RecipeList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList({ recipes }) {
+	return (
+		<div>
+			{recipes.map(recipe => <Recipe key={recipe.id} {...recipe} />)}
+		</div>
+	)
+}
