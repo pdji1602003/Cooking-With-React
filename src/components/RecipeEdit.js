5f861ee21f67729e4f1343af277667efbadddb36
pdji1602003 <pdji1602003@gmail.com>
2020-01-02 22:34:32 +0800

    mobile version

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -26,85 +26,88 @@ export default function RecipeEdit({ recipe }) {
 			amount: ''
 		}
 
-		handleChange({ingredients: [...recipe.ingredients, newIngredient]})
+		handleChange({ ingredients: [...recipe.ingredients, newIngredient] })
 	}
 
-	function handleIngredientDelete(id){
+	function handleIngredientDelete(id) {
 		handleChange({
-			ingredients:recipe.ingredients.filter(i => i.id !== id)
+			ingredients: recipe.ingredients.filter(i => i.id !== id)
 		})
 	}
 
 	return (
-		<div className="recipe-edit">
-			<div className="recipe-edit_delete-btn-container">
-				<button
-					className="btn"
-					onClick={handleDeleteButton}
-				>
-					&times;
+		<div className="recipe-overlay">
+			<div className="recipe-edit">
+				<div className="recipe-edit_delete-btn-container">
+					<button
+						className="btn"
+						onClick={handleDeleteButton}
+					>
+						&times;
 				</button>
-			</div>
-			<div className="recipe-edit_ingredient-grid">
-				<label className="recipe-edit_label" htmlFor="name">Recipe Name</label>
-				<input
-					className="recipe-edit_input"
-					type="text"
-					name="name"
-					id="name"
-					value={recipe.name}
-					onChange={(e) => handleChange({ name: e.target.value })}
-				/>
-				<label className="recipe-edit_label" htmlFor="cookTime">Cook Time</label>
-				<input
-					className="recipe-edit_input"
-					type="text"
-					name="cookTime"
-					id="cookTime"
-					value={recipe.cookTime}
-					onChange={(e) => handleChange({ cookTime: e.target.value })}
-				/>
-				<label className="recipe-edit_label" htmlFor="servings">Servings</label>
-				<input
-					className="recipe-edit_input"
-					type="number"
-					min="1"
-					name="servings"
-					id="servings"
-					value={recipe.servings}
-					onChange={(e) => handleChange({ servings: parseFloat(e.target.value) || "" })}
-				/>
-				<label className="recipe-edit_label" htmlFor="instructions">Instructions</label>
-				<textarea
-					className="recipe-edit_input recipe-edit_textarea"
-					name="instructions"
-					id="instructions"
-					value={recipe.instructions}
-					onChange={(e) => handleChange({ instructions: e.target.value })}
-				/>
-			</div>
-			<label className="recipe-edit_label">Ingredient</label>
-			<div className="recipe_edit-add-ingredient-grid">
-				<div className="recipe_edit_ingredient-heading">Name</div>
-				<div className="recipe_edit_ingredient-heading">Amount</div>
-				<div></div>
-				{recipe.ingredients.map(ingredient => (
-					<RecipeIngredientEdit
-						key={ingredient.id}
-						ingredient={ingredient}
-						handleIngredientChange={handleIngredientChange}
-						handleIngredientDelete={handleIngredientDelete}
-					/>)
-				)}
-			</div>
-			<div className="recipe-edit_add-ingredient-btn-container">
-				<button 
-					className="btn btn-primary"
-					onClick={handleIngredientAdd}
-				>
-				Add Ingredient
+				</div>
+				<div className="recipe-edit_ingredient-grid">
+					<label className="recipe-edit_label" htmlFor="name">Recipe Name</label>
+					<input
+						className="recipe-edit_input"
+						type="text"
+						name="name"
+						id="name"
+						value={recipe.name}
+						onChange={(e) => handleChange({ name: e.target.value })}
+					/>
+					<label className="recipe-edit_label" htmlFor="cookTime">Cook Time</label>
+					<input
+						className="recipe-edit_input"
+						type="text"
+						name="cookTime"
+						id="cookTime"
+						value={recipe.cookTime}
+						onChange={(e) => handleChange({ cookTime: e.target.value })}
+					/>
+					<label className="recipe-edit_label" htmlFor="servings">Servings</label>
+					<input
+						className="recipe-edit_input"
+						type="number"
+						min="1"
+						name="servings"
+						id="servings"
+						value={recipe.servings}
+						onChange={(e) => handleChange({ servings: parseFloat(e.target.value) || "" })}
+					/>
+					<label className="recipe-edit_label" htmlFor="instructions">Instructions</label>
+					<textarea
+						className="recipe-edit_input recipe-edit_textarea"
+						name="instructions"
+						id="instructions"
+						value={recipe.instructions}
+						onChange={(e) => handleChange({ instructions: e.target.value })}
+					/>
+				</div>
+				<label className="recipe-edit_label">Ingredient</label>
+				<div className="recipe_edit-add-ingredient-grid">
+					<div className="recipe_edit_ingredient-heading">Name</div>
+					<div className="recipe_edit_ingredient-heading">Amount</div>
+					<div></div>
+					{recipe.ingredients.map(ingredient => (
+						<RecipeIngredientEdit
+							key={ingredient.id}
+							ingredient={ingredient}
+							handleIngredientChange={handleIngredientChange}
+							handleIngredientDelete={handleIngredientDelete}
+						/>)
+					)}
+				</div>
+				<div className="recipe-edit_add-ingredient-btn-container">
+					<button
+						className="btn btn-primary"
+						onClick={handleIngredientAdd}
+					>
+						Add Ingredient
 				</button>
+				</div>
 			</div>
 		</div>
+
 	)
 }
